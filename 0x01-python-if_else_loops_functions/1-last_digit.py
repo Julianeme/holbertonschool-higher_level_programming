eea8b40ec22c6ad9d00ca46661dcf368631934f2
Julianeme <juliantve@gmail.com>
2021-04-27 15:10:29 -0500

    exit statement correction for negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 100000)
 if number < 0:
-    last_digit = (number *  (-1)) % 10
+    last_digit = (number * (-1)) % 10
 else:
     last_digit = number % 10
 if last_digit > 5:
