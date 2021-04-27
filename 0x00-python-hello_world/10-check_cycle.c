60644b3a67f6b43d32a219f280dd0e26fba2b1b9
Julianeme <juliantve@gmail.com>
2021-04-26 22:21:13 -0500

    libraries correction

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -22,7 +22,7 @@ int check_cycle(listint_t *list)
 	{
 		fast_ptr = fast_ptr->next->next;
 		slow_ptr = slow_ptr->next;
-		if ((fast_ptr == slow_ptr)
+		if (fast_ptr == slow_ptr)
 			return (1);
 	}
 	return (0);
