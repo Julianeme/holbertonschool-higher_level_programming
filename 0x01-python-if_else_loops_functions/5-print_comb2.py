bed19a64c8c7757efda67b436a42298c6a6afdff
Julianeme <juliantve@gmail.com>
2021-04-27 18:06:43 -0500

    00 added ad the beggining of the numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(00, 100):
     if num != 99:
-        print("{}".format(num), end=", ")
+        print("{:02d}".format(num), end=", ")
     else:
-        print("{}\n".format(num))
+        print("{:02d}\n".format(num))
