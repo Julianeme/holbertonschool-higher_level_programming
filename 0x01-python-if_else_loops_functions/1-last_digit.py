081544d5a5eaaa591ab74c589313515dab35e9d5
Julianeme <juliantve@gmail.com>
2021-04-27 15:09:57 -0500

    exit statement correction for negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 100000)
-last_digit = number % 10
+if number < 0:
+    last_digit = (number *  (-1)) % 10
+else:
+    last_digit = number % 10
 if last_digit > 5:
     print("Last digit of {:d} is {:d} and is \
-        greater than 5".format(number, number[:-1]))
+greater than 5".format(number, last_digit))
 elif last_digit == 0:
     print("Last digit of {:d} is {:d} and \
-        is 0".format(number, last_digit))
+is 0".format(number, last_digit))
 else:
     print(
         "Last digit of {:d} is {:d} and is less\
-        than 6 and not 0".format(number, last_digit))
+than 6 and not 0".format(number, last_digit))
