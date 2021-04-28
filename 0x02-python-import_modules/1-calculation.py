abf4cc5a2d3a545e52588ac8794a2197bcec1fe9
Julianeme <juliantve@gmail.com>
2021-04-28 13:35:57 -0500

    if __name__ conditional added

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
-c = add(a, b)
-print("{} + {} = {}".format(a, b, c))
-c = sub(a, b)
-print("{} - {} = {}".format(a, b, c))
-c = mul(a, b)
-print("{} * {} = {}".format(a, b, c))
-c = div(a, b)
-print("{} / {} = {}".format(a, b, c))
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    c = add(a, b)
+    print("{} + {} = {}".format(a, b, c))
+    c = sub(a, b)
+    print("{} - {} = {}".format(a, b, c))
+    c = mul(a, b)
+    print("{} * {} = {}".format(a, b, c))
+    c = div(a, b)
+    print("{} / {} = {}".format(a, b, c))
