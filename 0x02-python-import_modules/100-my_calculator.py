588fb6a58b8f02f2d22ff48a8d78ac69b2678a82
Julianeme <juliantve@gmail.com>
2021-04-28 17:59:07 -0500

    import declaration corrected

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import *
+    from calculator_1 import add, sub, mul, div
     import sys
     if len(sys.argv) != 4:
         print("print Usage: ./100-my_calculator.py <a> <operator> <b>")
