371504e788743d19d5d956eeb7d17d1237b5a3fc
Julianeme <juliantve@gmail.com>
2021-04-27 21:09:47 -0500

    uploading file for first time

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+uinit = 0
+for x in range(0, 10):
+    for y in range(uinit, 10):
+        print("{:d}{:d}" .format(x, y), end="")
+        if ((x == 8) and (y == 9)):
+            print()
+        else:
+            print(", ", end="")
+    uinit = uinit + 1
