af916ddf0be41634b33e7ef7f5e344ca58430fd9
Julianeme <juliantve@gmail.com>
2021-05-05 16:07:30 -0500

    line to initialize the variable -max- changed after checking the my_list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = my_list[0]
     if not my_list:
         return(None)
+    max = my_list[0]
     for x in my_list:
         if x > max:
             max = x
