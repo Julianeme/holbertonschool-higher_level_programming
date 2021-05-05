a87d6646a19a2b56441a270799e393f494fa0ddc
Julianeme <juliantve@gmail.com>
2021-05-05 16:05:53 -0500

    value of the variable used to store the max value changed to include negative numbers

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    a = 0
+    max = my_list[0]
     if not my_list:
         return(None)
     for x in my_list:
-        if x > a:
-            a = x
-    return(a)
+        if x > max:
+            max = x
+    return(max)
