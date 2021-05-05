8a3d46e5482965e7930f17f1be7b306257a6c3f0
Julianeme <juliantve@gmail.com>
2021-05-05 15:50:25 -0500

    verification added to check for valid sentence

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tuple_c = (0, 0)
-    tuple_c = (len(sentence), sentence[0])
-    return(tuple_c)
+    if sentence:
+        tuple_c = (0, 0)
+        tuple_c = (len(sentence), sentence[0])
+        return(tuple_c)
