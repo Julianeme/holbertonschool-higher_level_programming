001bea5a93e0d380ac1302ee7c3799e81bc74d81
Julianeme <juliantve@gmail.com>
2021-05-06 20:56:00 -0500

    list multiplier updated to var number

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    copy_list = list(map(lambda x: x * 2, my_list))
+    copy_list = list(map(lambda x: x * number, my_list))
     return(copy_list)
