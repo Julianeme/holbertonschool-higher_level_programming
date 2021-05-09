2b299a313184793ed1fa1af34c56d2268d5e42af
Julianeme <juliantve@gmail.com>
2021-05-09 10:50:14 -0500

    loop -for- changed for -while- loop

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    r_str = roman_string
     r_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
-    j = 1
     i = 0
-    if roman_string is None or type(roman_string) != str:
+    if r_str is None or type(r_str) != str:
         return(0)
-    if len(roman_string) == 1:
-        return(r_num[roman_string[i]])
-    for i in range(0, len(roman_string)):
-        if roman_string[j] and r_num[roman_string[i]] < r_num[roman_string[j]]:
-            total = total + r_num[roman_string[j]] - r_num[roman_string[i]]
-            j = j + 1
-            if j < len(roman_string):
-                i = i + 2
-                j = i + 1
-            else:
+    if len(r_str) == 1:
+        return(r_num[r_str[i]])
+    while i < len(r_str):
+        j = i + 1
+        if j < len(r_str) and r_num[r_str[i]] < r_num[r_str[j]]:
+            total = total + r_num[r_str[j]] - r_num[r_str[i]]
+            i = i + 1
+            if j >= len(r_str) - 1:
                 return total
         else:
-            total = total + r_num[roman_string[i]]
-            j = i + 1
+            total = total + r_num[r_str[i]]
+        i += 1
     return(total)
