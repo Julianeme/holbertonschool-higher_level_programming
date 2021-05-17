89cf2ccf365a5c4ff4ba0c3453ceac4fd74a271c
Julianeme <juliantve@gmail.com>
2021-05-17 18:41:18 -0500

    shebang added

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -12,6 +12,7 @@ def list_division(my_list_1, my_list_2, list_length):
             res = 0
             print("division by 0")
         except IndexError:
+            res = 0
             print("out of range")
         finally:
             res_list.append(res)
