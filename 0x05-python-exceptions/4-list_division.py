a896c740cfc0542b4459ce374176eed4d6d0cc85
Julianeme <juliantve@gmail.com>
2021-05-17 18:37:50 -0500

    uploading file for first time

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from types import resolve_bases
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    res_list = []
+    res = 0
+    for i in range(list_length):
+        try:
+            res = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            res = 0
+            print("wrong type")
+        except ZeroDivisionError:
+            res = 0
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+        finally:
+            res_list.append(res)
+    return res_list
