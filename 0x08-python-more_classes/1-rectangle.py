04c7483879fefa8c29d06c822641f29489e0b339
Julianeme <juliantve@gmail.com>
2021-05-20 18:54:10 -0500

    value verification conditionals for width and height corrected

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -18,12 +18,12 @@ class Rectangle:
         @width.setter
         def width(self, value):
             '''setter, checks for the right tipe of widht'''
-            if type(value) is int:
-                self.__width = value
-            elif type(value) is not int:
+            if type(value) is not int:
                 raise TypeError("width must be an integer")
             elif value < 0:
                 raise ValueError("width must be >= 0")
+            else:
+                self.__width = value
 
         @property
         def height(self):
@@ -33,9 +33,9 @@ class Rectangle:
         @height.setter
         def height(self, value):
             '''checks that height has a valid value'''
-            if type(value) is int:
-                self.__height = value
-            elif type(value) is not int:
+            if type(value) is not int:
                 raise TypeError("height must be an integer")
             elif value < 0:
                 raise ValueError("height must be >= 0")
+            else:
+                self.__height = value
