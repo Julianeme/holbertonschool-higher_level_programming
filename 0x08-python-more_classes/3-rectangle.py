67b5c26da320f910b7eee87c53d7c7ccb7be0b6c
Julianeme <juliantve@gmail.com>
2021-05-20 20:14:00 -0500

    comments added for area and perimeter methods

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -40,9 +40,11 @@ class Rectangle:
             self.__height = value
 
     def area(self):
+        '''returns the area of the rectangle'''
         return self.__height * self.__width
 
     def perimeter(self):
+        '''returns the perimeter of the rectangle'''
         if self.__width == 0 or self.__height == 0:
             return 0
         else:
