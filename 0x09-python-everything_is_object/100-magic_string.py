543f632d73bb6bf52f38e0623214fbebec80850e
Julianeme <juliantve@gmail.com>
2021-05-21 21:13:42 -0500

    counter changed to list

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    magic_string.counter += 1
-    return ("Holberton " + (", Holberton" * (magic_string.counter - 1)))
-magic_string.counter = 0
+def magic_string(counter=[]):
+    counter += ["Holberton"]
+    return (", ".join(counter))
