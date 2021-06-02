5481159b3e106928ed05ea4c475f8ab65afd3e24
Julianeme <juliantve@gmail.com>
2021-06-01 19:58:21 -0500

    code order changed

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,16 +7,16 @@
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Square (Rectangle):
+class Square(Rectangle):
     """
         instances area and str magic funtions added
 
     """
 
     def __init__(self, size):
-        super().__init__(size, size)
-        self.__size = size
         self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         return self.__size ** 2
