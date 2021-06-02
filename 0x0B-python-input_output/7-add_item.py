d24f5e4478707edc12ae39ac07c98c42e39b0001
Julianeme <juliantve@gmail.com>
2021-06-02 18:13:56 -0500

    tye - except added to append elements in the list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,6 +10,10 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-save_to_json_file(sys.argv[1:], "add_item.json")
-load_from_json_file("add_item.json")
+try:
+    a = load_from_json_file("add_item.json")
+except:
+    a = []
+for element in sys.argv[1:]:
+    a.append(element)
+save_to_json_file(a, "add_item.json")
