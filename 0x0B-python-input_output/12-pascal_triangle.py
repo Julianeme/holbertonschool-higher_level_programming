84a96e89918bb086bb8f8c24b9c54a41ff3f4783
Julianeme <juliantve@gmail.com>
2021-06-02 23:59:43 -0500

    uploading files for first time

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    function def pascal_triangle(n): that returns a list of
+    lists of integers representing the Pascal’s triangle of n
+
+"""
+
+
+def pascal_triangle(n):
+    """
+        instances area and str magic funtions added
+
+    """
+    if n <= 0:
+        return ""
+    else:
+        print("1")
