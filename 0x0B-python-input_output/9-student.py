1e19d5ea9fca9bd916815e3808c08cd1a2969ffa
Julianeme <juliantve@gmail.com>
2021-06-02 20:55:29 -0500

    uploading file for first time

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    Base class BaseGeometry and and subclass Rectangle
+
+
+"""
+
+
+class Student:
+    """
+        instances area and str magic funtions added
+
+    """
+
+    def __init__(self, first_name, last_name, age):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return (self.__dict__)
