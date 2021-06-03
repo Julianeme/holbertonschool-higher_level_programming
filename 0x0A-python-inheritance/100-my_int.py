af64fc22d5eeeb3faf235d2ad1cc843d9ce94d9b
Julianeme <juliantve@gmail.com>
2021-06-02 23:50:08 -0500

    uploading file for first time

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3
+"""
+    Class MyInt derived from int
+
+"""
+
+
+class MyInt(int):
+    """
+    equals and diff works the other way around
+
+    """
+    def __bol__(self, num):
+        self.num = num
+        return False
