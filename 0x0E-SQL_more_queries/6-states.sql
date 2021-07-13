d633390dd44b884eb36151b8652fdda6e4aee482
Julianeme <juliantve@gmail.com>
2021-07-13 18:59:15 -0500

    6 | code update

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,5 +2,4 @@
 -- (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-state_id INT NOT NULL states(id), name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
