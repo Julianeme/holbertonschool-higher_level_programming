6b79f0dec9404f91999d86899c661a0dd8fdc756
Julianeme <juliantve@gmail.com>
2021-07-13 17:03:06 -0500

    ...

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -4,5 +4,6 @@
 
 CREATE IF NOT EXISTS USER 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
