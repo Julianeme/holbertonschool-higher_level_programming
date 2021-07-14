5d05103ee356fbc73c3b1b49147156ba8c3ed373
Julianeme <juliantve@gmail.com>
2021-07-13 19:35:01 -0500

    7 | foreing key statement added

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,5 +2,5 @@
 -- (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-state_id INT NOT NULL, name VARCHAR(256) NOT NULL, FOREING KEY (state_id) REFERENCES hbtn_0d_usa.states(id));
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+state_id INT NOT NULL, name VARCHAR(256) NOT NULL, FOREING KEY (state_id) REFERENCES hbtn_0d_usa(states(id)));
