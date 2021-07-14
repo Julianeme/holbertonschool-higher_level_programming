a2a8e37b06ec0915597a956c44b7149faaf50f3d
Julianeme <juliantve@gmail.com>
2021-07-13 22:59:14 -0500

    | uploading file for first time

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,3 @@
+-- Write a script that lists all shows contained in hbtn_0d_tvshows
+-- that have at least one genre linked
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id FROM tv_shows INNER JOIN tv_show_genres ON show.id = tv_genres.id ORDER BY tv_shows.title, tv_show_genres.genre_id;
