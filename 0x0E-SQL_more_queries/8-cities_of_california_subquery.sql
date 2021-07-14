d3ffe12fae9e9893c2d23fcaacd1920823f60cec
Julianeme <juliantve@gmail.com>
2021-07-13 20:51:07 -0500

    8 | style correction

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,8 @@
 -- script that lists all the cities of California that can be
-SELECT id, name
+SELECT id, name 
 FROM cities
 WHERE states_id =
-(SELECT id
-FROM states
-WHERE name = "California")
+	(SELECT id
+	FROM states
+	WHERE name = "California")
 ORDER BY id ASC;
