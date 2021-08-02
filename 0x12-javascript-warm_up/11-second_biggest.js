a4cba457dc2a5aabb345065b0554cbd6e855e81a
Julianeme <juliantve@gmail.com>
2021-08-02 17:24:56 -0500

    uploading file for first time

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const args = process.argv;
+let biggest = process.argv[2];
+let secondBiggest = process.argv[2];
+if (args.length > 3) {
+  for (let i = 3; i < args.lenght; i++) {
+    if (biggest > args[i]) {
+      biggest = args[i];
+    }
+  } for (let j = 3; j < args.lenght; j++) {
+    if (secondBiggest > args[j] && secondBiggest < biggest) {
+      secondBiggest = args[j];
+    }
+  }
+} else {
+  secondBiggest = 0;
+}
+console.log(secondBiggest);
