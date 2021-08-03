b705795bcc1adcc94a593941bc5749b3ba976a21
Julianeme <juliantve@gmail.com>
2021-08-03 15:29:47 -0500

    patter defined a this.pattern = c

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -10,7 +10,7 @@ module.exports = class Square extends Rectangle {
   charPrint (c) {
     let side = 0;
     this.pattern = c;
-    if (c !== 'c' && c !== 'C') {
+    if (this.pattern !== 'c' && this.pattern !== 'C') {
       this.pattern = 'X';
     } while (side < this.width) {
       console.log(this.pattern.repeat(this.width));
