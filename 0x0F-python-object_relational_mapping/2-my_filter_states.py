1b2905e798f0da8efdeeae711f257b1e2f48eb7d
Julianeme <juliantve@gmail.com>
2021-08-06 15:07:23 -0500

    uploading file for first time

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import MySQLdb
+from sys import argv
+# Open database connection
+db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                     password=argv[2], db=argv[3], charset="utf8")
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+try:
+    # Execute the SQL command
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY id".
+                   format(argv[4]))
+    # Fetch all the rows in a list of lists.
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+except:
+    print("Error: unable to fecth data")
+
+# disconnect from server
+cursor.close()
+db.close()
