dbdce2374f138ba9f24304055a20d14ddbf089b6
Julianeme <juliantve@gmail.com>
2021-08-06 10:46:28 -0500

    uploading file for first time

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import MySQLdb
+from sys import argv
+# Open database connection
+db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                     password=argv[2], db=argv[3], charset="utf8")
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+sql = "SELECT * FROM states ORDER BY id ASC"
+try:
+    # Execute the SQL command
+    cursor.execute(sql)
+    # Fetch all the rows in a list of lists.
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+except:
+    print("Error: unable to fecth data")
+
+# disconnect from server
+cursor.close()
+db.close()
