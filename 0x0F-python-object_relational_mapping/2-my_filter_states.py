d38f6a3af6643a1242f01ac3a13db59a3eaabd9c
Julianeme <juliantve@gmail.com>
2021-08-07 15:09:12 -0500

    query search updated to add Binary condition

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 
     try:
         # Execute the SQL command
-        cursor.execute("SELECT * FROM states WHERE name LIKE BINARY {}\
+        cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
                        ORDER BY id".format(argv[4]))
         # Fetch all the rows in a list of lists.
         results = cursor.fetchall()
