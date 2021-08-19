7b4c835a208bf54a8004b495f3de792d14efdf1b
Julianeme <juliantve@gmail.com>
2021-08-19 17:19:27 -0500

    sintaxis changed

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will accept
-curl -sI "$1" | grep -i Allow | awk '{print "@F[1..$#F]"}'
+curl -sI "$1" | grep -i Allow | pearl -lane '{print "@F[1..$#F]"}'
