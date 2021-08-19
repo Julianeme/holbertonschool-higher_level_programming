1a0a97d7bc20a4e23daf53bff471b740cee887ec
Julianeme <juliantve@gmail.com>
2021-08-19 17:16:18 -0500

    uploading file for first time

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL and displays
-# all HTTP methods the server will accept
-
-for method in OPTIONS GET HEAD POST PUT DELETE TRACE CONNECT ; do 
-    echo -e "\n\nTrying $method\n\n" 
-    echo -e "$method / HTTP/1.1\nHost: server-hostname\nConnection: close\n\n" | nc server-hostname 80 | head 
-    sleep 2
-done
+# Bash script that takes in a URL and displays all HTTP methods the server will accept
+curl -sI "$1" | grep -i Allow | awk '{print "@F[1..$#F]"}'
