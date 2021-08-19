b311d72ea8730ee83e6ff6614634de5f1743768b
Julianeme <juliantve@gmail.com>
2021-08-19 16:16:51 -0500

    sintaxis changed

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # script that takes in a URL, sends a request to it,and displays its size
 
- Content-Length: "curl -s http://$1"
+curl -sI "http://$1" | grep -i content-length | awk '{print $2}'
