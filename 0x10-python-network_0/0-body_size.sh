0ebd70b9bb2f39264f2ffd5732753f092bfc22c5
Julianeme <juliantve@gmail.com>
2021-08-19 15:45:18 -0500

    sintaxis modification

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
+# script that takes in a URL, sends a request to it,and displays its size
 
-curl -s Content-Length: "http://$1"
+Content-Length: curl -s "http://$1"
