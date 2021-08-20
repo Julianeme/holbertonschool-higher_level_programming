a83b95b6b756a46a450cb0593de704172b41c8f8
Julianeme <juliantve@gmail.com>
2021-08-19 21:57:17 -0500

    uploading file for first time

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,23 @@
+#!/usr/Defbin/python3
+"""
+finds first peak in list
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    function that finds a peak in a list of unsorted integers.
+    """
+    x = 0
+    while x < len(list_of_integers):
+        if x == 0 and list_of_integers[x + 1] < list_of_integers[0]:
+            return (list_of_integers[x])
+        elif [x + 1] == len(list_of_integers) - 1 and\
+                list_of_integers[x] < list_of_integers[x + 1]:
+            return list_of_integers[x + 1]
+        elif (x + 1) < len(list_of_integers) and list_of_integers[x - 1] \
+                <= list_of_integers[x] and\
+                list_of_integers[x] > list_of_integers[x + 1]:
+            return list_of_integers[x]
+        else:
+            x += 1
