db2f3a10b982d59154fd7420c0124fdc6a6154ac
Julianeme <juliantve@gmail.com>
2021-08-20 14:45:23 -0500

    identation corrected

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,8 +7,8 @@ import urllib.request
 
 if __name__ == "__main__":
 
-with urllib.request.urlopen("https://intranet.hbtn.io/status/") as response:
-    html = response.read()
-    print("- type: {}".format(type(html)))
-    print("content: {}".format(html))
-    print("- utf8 content: {}".format(html.decode("utf-8")))
+    with urllib.request.urlopen("https://intranet.hbtn.io/status/") as response:
+        html = response.read()
+        print("- type: {}".format(type(html)))
+        print("content: {}".format(html))
+        print("- utf8 content: {}".format(html.decode("utf-8")))
