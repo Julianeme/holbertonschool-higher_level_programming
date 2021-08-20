5871e2dcd264371f72b6736e4423951e5e3acf37
Julianeme <juliantve@gmail.com>
2021-08-20 15:16:44 -0500

    url corrected to args

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,6 +9,5 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    with urllib.request.urlopen("https://intranet.hbtn.io/status/") \
-            as response:
-        print("{}".format(response.getheader('X-Request-Id')))
+    with urllib.request.urlopen(argv[1]) as response:
+        print(response.getheader('X-Request-Id'))
