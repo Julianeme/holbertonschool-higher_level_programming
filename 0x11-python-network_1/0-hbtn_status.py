3ca332d5be7da97df5617ca881ae495d189b4e55
Julianeme <juliantve@gmail.com>
2021-08-20 14:44:26 -0500

    no execution condition added

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,6 +5,8 @@ script that fetches https://intranet.hbtn.io/status
 
 import urllib.request
 
+if __name__ == "__main__":
+
 with urllib.request.urlopen("https://intranet.hbtn.io/status/") as response:
     html = response.read()
     print("- type: {}".format(type(html)))
