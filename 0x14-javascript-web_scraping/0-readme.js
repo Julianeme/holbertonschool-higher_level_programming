603a58255cae368b1df68252238465f3a7be5c22
Julianeme <juliantve@gmail.com>
2021-09-14 11:52:28 -0500

    code added

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,11 @@
-dafdafd
+#!/usr/bin/node
+
+const fs = require('fs');
+// __dirname means relative to script. Use "./data.txt" if you want it relative to execution path.
+fs.readFile(process.argv[2], 'utf8', (error, data) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(data);
+});
