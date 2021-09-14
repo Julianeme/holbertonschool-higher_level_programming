bf51dc3428dc29aa586858c7952e86ca1f499ee4
Julianeme <juliantve@gmail.com>
2021-09-14 13:34:32 -0500

    uploading file for first time

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+ *script that writes a string to a file.
+ */
+const request = require('request');
+request(process.argv[2], function (err, res) {
+  if (err) return console.log('Error: ', err);
+  console.log('Status: %d', res.statusCode);
+});
