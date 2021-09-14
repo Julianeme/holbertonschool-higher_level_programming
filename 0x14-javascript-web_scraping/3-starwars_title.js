a3276d023b62bc6280511708c8943a9bc14eda6e
Julianeme <juliantve@gmail.com>
2021-09-14 18:33:00 -0500

    going back to previous commit

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,11 +5,9 @@
 const request = require('request');
 const rUrl = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(rUrl, function (err, res, body) {
-  if (err) {
-    return console.log('Error: ', err);
-  } else if (res.statusCode === 200) {
-    const chars = JSON.parse(body).title;
-    for (character of chars){
-      console.log("character is: %s", character)}
-  }
+	if (err) {
+		return console.log('Error: ', err);
+	} else if (res.statusCode === 200) {
+		console.log('%s', JSON.parse(body).title);
+	}
 });
