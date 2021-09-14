3c2de1963426a085e489194176438cb47e229b35
Julianeme <juliantve@gmail.com>
2021-09-14 14:36:18 -0500

    elseif condition modified

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,11 +3,11 @@
  *script that writes a string to a file.
  */
 const request = require('request');
-const r_url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2]
-request(r_url, function (err, res, body) {
-  if (err) {return console.log('Error: ', err);
-  } else if (JSON.parse(body).episode_id == process.argv[2]) {
-
-    console.log('Episodio: %s', JSON.parse(body).title);
+const rUrl = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(rUrl, function (err, res, body) {
+  if (err) {
+    return console.log('Error: ', err);
+  } else if (res.statusCode === 200) {
+    console.log('%s', JSON.parse(body).title);
   }
 });
