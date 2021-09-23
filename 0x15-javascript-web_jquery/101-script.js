04a14320359fd67b667008343d5e7fab3fc93ec9
Julianeme <juliantve@gmail.com>
2021-09-22 20:07:35 -0500

    100 - 101 | uploading files for first time

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('#add_item').click(function () {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+  $('#remove_item').click(function () {
+    $('UL.my_list li:last-child').remove();
+  });
+  $('#clear_list').click(function () {
+    $('UL.my_list').empty('<li>Item</li>');
+  });
+})();
